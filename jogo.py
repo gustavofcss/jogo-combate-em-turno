16ec142f92ead7e96b6f457ec10bd4f40d8a3b6b
Gustavo Felipe <gustavofcssantos@outlook.com>
2026-04-07 22:08:23 -0300

    Criando a classe do herói e a do inimigo

---
+++ jogo.py
@@ -0,0 +1,31 @@
+class Personagem:
+    def __init__(self, nome, vida, nivel):
+        self.__nome = nome
+        self.__vida = vida
+        self.__nivel = nivel
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_vida(self):
+        return self.__vida
+    
+    def get_nivel(self):
+        return self.__nivel
+    
+    
+class Heroi(Personagem):
+    def __init__(self, nome, vida, nivel, habilidade):
+        super().__init__(nome, vida, nivel)
+        self.__habilidade = habilidade
+
+        def get_habilidade(self):
+            return self.__habilidade
+        
+class Inimigo(Personagem):
+    def __init__(self, nome, vida, nivel, tipo):
+        super().__init__(nome, vida, nivel)
+        self.__tipo = tipo
+
+    def get_tipo(self):
+        return self.__tipo
