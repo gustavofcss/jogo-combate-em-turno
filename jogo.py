eb0a5248569c1d24d119ef794a39313417112ec1
Gustavo Felipe <gustavofcssantos@outlook.com>
2026-04-08 00:19:25 -0300

    organizando as funções de ataque e de receber o ataque

--- jogo.py
+++ jogo.py
@@ -16,6 +16,16 @@ class Personagem:
     def exibir_detalhes(self):
         return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
     
+    def atacar(self, alvo):
+         dano = self.__nivel * 2
+         alvo.receber_ataque(dano)
+         print(f"{self.get_nome()} atacou {alvo.get_nome()} causando {dano} de dano!")
+
+    def receber_ataque(self, dano):
+        self.__vida -= dano
+        if self.__vida <= 0:
+             self.__vida = 0
+
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
         super().__init__(nome, vida, nivel)
@@ -43,9 +53,9 @@ class Jogo:
         self.heroi = Heroi("Arqueiro", 100, 5, "Tiro Certeiro")
         self.inimigo = Inimigo("Goblin", 50, 3, "Verde")
       
-    def inciar_bbatalha(self):
+    def inciar_batalha(self):
         print("Iniciado batalha!")
-        while self.heroi.get_vida > 0 and self.inimigo.get_vida() > 0:
+        while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
              print("\n Detalhes dos Personagens:")
              print(self.heroi.exibir_detalhes())
              print(self.inimigo.exibir_detalhes())
@@ -53,5 +63,10 @@ class Jogo:
              input("Pressione Enter para atacar...")
              escolha = input("Escolha (1 - Ataque Normal, 2 - Ataque Especial): ")
 
+             if escolha == "1":
+                 self.heroi.atacar(self.inimigo)
+             else:
+                  print("Escolha inválida. Escolha novamente.")
+                  
 jogo = Jogo()
-jogo.inciar_bbatalha()
+jogo.inciar_batalha()
